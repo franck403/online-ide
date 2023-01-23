26dac366e4251d4446b936e774de90688dfe6b17
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-23 13:35:54 -0500

    Update ide.js

--- ui/js/ide.js
+++ ui/js/ide.js
@@ -2,25 +2,25 @@ let editor;
 
 window.onload = function() {
     editor = ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.session.setMode("ace/mode/c_cpp");
+    editor.setTheme("https://franck403.github.io/online-ide/ace/theme/monokai");
+    editor.session.setMode("https://franck403.github.io/online-ide/ace/mode/c_cpp");
 }
 
 function changeLanguage() {
 
     let language = $("#languages").val();
 
-    if(language == 'c' || language == 'cpp')editor.session.setMode("ace/mode/c_cpp");
-    else if(language == 'php')editor.session.setMode("ace/mode/php");
-    else if(language == 'python')editor.session.setMode("ace/mode/python");
-    else if(language == 'node')editor.session.setMode("ace/mode/javascript");
+    if(language == 'c' || language == 'cpp')editor.session.setMode("https://franck403.github.io/online-ide/ace/mode/c_cpp");
+    else if(language == 'php')editor.session.setMode("https://franck403.github.io/online-ide/ace/mode/php");
+    else if(language == 'python')editor.session.setMode("https://franck403.github.io/online-ide/ace/mode/python");
+    else if(language == 'node')editor.session.setMode("https://franck403.github.io/online-ide/ace/mode/javascript");
 }
 
 function executeCode() {
 
     $.ajax({
 
-        url: "/ide/app/compiler.php",
+        url: "https://franck403.github.io/online-ide/ide/app/compiler.php",
 
         method: "POST",
 
